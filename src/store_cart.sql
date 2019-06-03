94f4a7d2030d348d9af71142ccb666160feccfb6
Zephon-H <1528498238@qq.com>
2019-06-03 16:07:49 +0800

    add database

--- src/store_cart.sql
+++ src/store_cart.sql
@@ -1,1 +1,11 @@
+
+create table store_cart
+(
+    cid          int(10) auto_increment
+        primary key,
+    goodsMapJson varchar(100)  null,
+    num          int default 0 null,
+    uid          int(10)       null
+);
+
 INSERT INTO JavaMysql0.store_cart (cid, goodsMapJson, num, uid) VALUES (4, '{"id":1,"name":"1号商品","price":300.0,"detail":"详细信息","type":"类型一","picture":"1","sales":0}', 3, 2);
